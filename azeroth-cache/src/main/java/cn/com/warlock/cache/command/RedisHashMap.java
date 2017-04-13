ebca7bd9250ee03006ad007ddf9a369aed1d124f
zhiye <zhiye.cb@antfin.com>
2017-04-13 17:55:32 +0800

    redis sth optimize

--- azeroth-cache/src/main/java/cn/com/warlock/cache/command/RedisHashMap.java
+++ azeroth-cache/src/main/java/cn/com/warlock/cache/command/RedisHashMap.java
@@ -1,217 +1,241 @@
 package cn.com.warlock.cache.command;
 
+import static cn.com.warlock.cache.redis.JedisProviderFactory.getBinaryJedisClusterCommands;
+import static cn.com.warlock.cache.redis.JedisProviderFactory.getBinaryJedisCommands;
+import static cn.com.warlock.cache.redis.JedisProviderFactory.getJedisProvider;
+import static cn.com.warlock.cache.redis.JedisProviderFactory.isCluster;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import redis.clients.util.SafeEncoder;
 
-import java.util.*;
-
-import static cn.com.warlock.cache.redis.JedisProviderFactory.*;
-
-
+/**
+ * 
+ * @author warlock
+ * @version $Id: RedisHashMap.java, v 0.1 Apr 13, 2017 5:45:15 PM warlock Exp $
+ */
 public class RedisHashMap extends RedisCollection {
 
-	public RedisHashMap(String key) {
-		super(key);
-	}
-	
-	/**
-	 * @param key
-	 * @param expireTime 超时时间(秒) 小于等于0 为永久缓存
-	 */
-	public RedisHashMap(String key,long expireTime) {
-		super(key,expireTime);
-	}
+    public RedisHashMap(String key) {
+        super(key);
+    }
 
-	/**
-	 * 指定组名
-	 * 
-	 * @param key
-	 * @param groupName
-	 */
-	public RedisHashMap(String key, String groupName) {
-		super(key, groupName);
-	}
-	
-	/**
-	 * 
-	 * @param key
-	 * @param groupName 分组名
-	 * @param expireTime 超时时间(秒) 小于等于0 为永久缓存
-	 */
-	public RedisHashMap(String key,String groupName,long expireTime) {
-		super(key,groupName,expireTime);
-	}
+    /**
+     * @param key
+     * @param expireTime 超时时间(秒) 小于等于0 为永久缓存
+     */
+    public RedisHashMap(String key, long expireTime) {
+        super(key, expireTime);
+    }
 
-	/**
-	 * 设置hash缓存
-	 * 
-	 * @param datas
-	 * @return
-	 */
-	public <T> boolean set(Map<String, T> datas) {
-		if(datas == null || datas.isEmpty())return false;
-		Map<byte[], byte[]> newDatas = new HashMap<>();
-		Set<String> keySet = datas.keySet();
-		for (String key : keySet) {
-			if(datas.get(key) == null)continue;
-			newDatas.put(SafeEncoder.encode(key), valueSerialize(datas.get(key)));
-		}
+    /**
+     * 指定组名
+     * 
+     * @param key
+     * @param groupName
+     */
+    public RedisHashMap(String key, String groupName) {
+        super(key, groupName);
+    }
 
-		boolean result = false;
-		try {
-			if (isCluster(groupName)) {
-				result = getBinaryJedisClusterCommands(groupName).hmset(key, newDatas).equals(RESP_OK);
-			} else {
-				result = getBinaryJedisCommands(groupName).hmset(key, newDatas).equals(RESP_OK);
-			}
-			//设置超时时间
-			if(result)setExpireIfNot(expireTime);
-			return result;
-		} finally {
-			getJedisProvider(groupName).release();
-		}
+    /**
+     * 
+     * @param key
+     * @param groupName 分组名
+     * @param expireTime 超时时间(秒) 小于等于0 为永久缓存
+     */
+    public RedisHashMap(String key, String groupName, long expireTime) {
+        super(key, groupName, expireTime);
+    }
 
-	}
-	
-	/**
-	 * 获取所有值
-	 * 
-	 * @return
-	 */
-	public <T> Map<String, T> getAll() {
-		try {
-			Map<byte[], byte[]> datas = null;
-			Map<String, T> result = new HashMap<>();
-			if (isCluster(groupName)) {
-				datas = getBinaryJedisClusterCommands(groupName).hgetAll(key);
-			} else {
-				datas = getBinaryJedisCommands(groupName).hgetAll(key);
-			}
+    /**
+     * 设置hash缓存
+     * 
+     * @param datas
+     * @return
+     */
+    public <T> boolean set(Map<String, T> datas) {
+        if (datas == null || datas.isEmpty())
+            return false;
+        Map<byte[], byte[]> newDatas = new HashMap<>();
+        Set<String> keySet = datas.keySet();
+        for (String key : keySet) {
+            if (datas.get(key) == null)
+                continue;
+            newDatas.put(SafeEncoder.encode(key), valueSerialize(datas.get(key)));
+        }
 
-			Iterator<Map.Entry<byte[], byte[]>> it = datas.entrySet().iterator();
-			while(it.hasNext()){
-				Map.Entry<byte[], byte[]> entry=it.next();
-				result.put(SafeEncoder.encode(entry.getKey()), (T)valueDerialize(entry.getValue()));
-			}
-			return result;
-		} finally {
-			getJedisProvider(groupName).release();
-		}
+        boolean result = false;
+        try {
+            if (isCluster(groupName)) {
+                result = getBinaryJedisClusterCommands(groupName).hmset(key, newDatas)
+                    .equals(RESP_OK);
+            } else {
+                result = getBinaryJedisCommands(groupName).hmset(key, newDatas).equals(RESP_OK);
+            }
+            //设置超时时间
+            if (result)
+                setExpireIfNot(expireTime);
+            return result;
+        } finally {
+            getJedisProvider(groupName).release();
+        }
 
-	}
+    }
 
-	/**
-	 * 查看缓存hash是否包含某个key
-	 * 
-	 * @param field
-	 * @return
-	 */
-	public boolean containsKey(String field) {
-		try {
-			if (isCluster(groupName)) {
-				return getBinaryJedisClusterCommands(groupName).hexists(key, SafeEncoder.encode(field));
-			} else {
-				return getBinaryJedisCommands(groupName).hexists(key, SafeEncoder.encode(field));
-			}
-		} finally {
-			getJedisProvider(groupName).release();
-		}
-	}
+    /**
+     * 获取所有值
+     * 
+     * @return
+     */
+    public <T> Map<String, T> getAll() {
+        try {
+            Map<byte[], byte[]> datas = null;
+            Map<String, T> result = new HashMap<>();
+            if (isCluster(groupName)) {
+                datas = getBinaryJedisClusterCommands(groupName).hgetAll(key);
+            } else {
+                datas = getBinaryJedisCommands(groupName).hgetAll(key);
+            }
 
-	/**
-	 * 设置ç
-	 * 
-	 * @param field
-	 * @param value
-	 * @return
-	 */
-	public boolean set(String field, Object value) {
-		boolean result = false;
-		if(value == null)return false;
-		//返回值（1:新字段被设置,0:已经存在值被更新）
-		try {
-			if (isCluster(groupName)) {
-				result = getBinaryJedisClusterCommands(groupName)
-						.hset(key, SafeEncoder.encode(field), valueSerialize(value)) >= 0;
-			} else {
-				result = getBinaryJedisCommands(groupName).hset(key, SafeEncoder.encode(field), valueSerialize(value)) >= 0;
-			}		
-			//设置超时时间
-			if(result)setExpireIfNot(expireTime);
-			return result;
-		} finally {
-			getJedisProvider(groupName).release();
-		}
-	}
+            Iterator<Map.Entry<byte[], byte[]>> it = datas.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry<byte[], byte[]> entry = it.next();
+                result.put(SafeEncoder.encode(entry.getKey()),
+                    (T) valueDerialize(entry.getValue()));
+            }
+            return result;
+        } finally {
+            getJedisProvider(groupName).release();
+        }
 
-	/**
-	 * 移除hash缓存中的指定值
-	 * 
-	 * @param field
-	 * @return
-	 */
-	public boolean remove(String field) {
-		try {
-			if (isCluster(groupName)) {
-				return getBinaryJedisClusterCommands(groupName).hdel(key, SafeEncoder.encode(field)).equals(RESP_OK);
-			} else {
-				return getBinaryJedisCommands(groupName).hdel(key, SafeEncoder.encode(field)).equals(RESP_OK);
-			}
-		} finally {
-			getJedisProvider(groupName).release();
-		}
-	}
+    }
 
-	/**
-	 * 返回长度
-	 * 
-	 * @return
-	 */
-	public long length() {
-		try {
-			if (isCluster(groupName)) {
-				return getBinaryJedisClusterCommands(groupName).hlen(key);
-			} else {
-				return getBinaryJedisCommands(groupName).hlen(key);
-			}
-		} finally {
-			getJedisProvider(groupName).release();
-		}
-	}
+    /**
+     * 查看缓存hash是否包含某个key
+     * 
+     * @param field
+     * @return
+     */
+    public boolean containsKey(String field) {
+        try {
+            if (isCluster(groupName)) {
+                return getBinaryJedisClusterCommands(groupName).hexists(key,
+                    SafeEncoder.encode(field));
+            } else {
+                return getBinaryJedisCommands(groupName).hexists(key, SafeEncoder.encode(field));
+            }
+        } finally {
+            getJedisProvider(groupName).release();
+        }
+    }
 
-	/**
-	 * 获取一个值
-	 * 
-	 * @param field
-	 * @return
-	 */
-	@SuppressWarnings("unchecked")
-	public <T> T getOne(String field) {
-		return (T) get(field).get(field);
-	}
+    /**
+     * 设置ç
+     * 
+     * @param field
+     * @param value
+     * @return
+     */
+    public boolean set(String field, Object value) {
+        boolean result = false;
+        if (value == null)
+            return false;
+        //返回值（1:新字段被设置,0:已经存在值被更新）
+        try {
+            if (isCluster(groupName)) {
+                result = getBinaryJedisClusterCommands(groupName).hset(key,
+                    SafeEncoder.encode(field), valueSerialize(value)) >= 0;
+            } else {
+                result = getBinaryJedisCommands(groupName).hset(key, SafeEncoder.encode(field),
+                    valueSerialize(value)) >= 0;
+            }
+            //设置超时时间
+            if (result)
+                setExpireIfNot(expireTime);
+            return result;
+        } finally {
+            getJedisProvider(groupName).release();
+        }
+    }
 
-	/**
-	 * 获取多个key的值
-	 * 
-	 * @param fields
-	 * @return
-	 */
-	public <T> Map<String, T> get(String... fields) {
-		try {
-			List<byte[]> datas = null;
-			Map<String, T> result = new HashMap<>();
-			for (String field : fields) {
-				if (isCluster(groupName)) {
-					datas = getBinaryJedisClusterCommands(groupName).hmget(key, SafeEncoder.encode(field));
-				} else {
-					datas = getBinaryJedisCommands(groupName).hmget(key, SafeEncoder.encode(field));
-				}
-				result.put(field, (T)valueDerialize(datas.get(0)));
-			}
-			return result;
-		} finally {
-			getJedisProvider(groupName).release();
-		}
+    /**
+     * 移除hash缓存中的指定值
+     * 
+     * @param field
+     * @return
+     */
+    public boolean remove(String field) {
+        try {
+            if (isCluster(groupName)) {
+                return getBinaryJedisClusterCommands(groupName).hdel(key, SafeEncoder.encode(field))
+                    .equals(RESP_OK);
+            } else {
+                return getBinaryJedisCommands(groupName).hdel(key, SafeEncoder.encode(field))
+                    .equals(RESP_OK);
+            }
+        } finally {
+            getJedisProvider(groupName).release();
+        }
+    }
 
-	}
+    /**
+     * 返回长度
+     * 
+     * @return
+     */
+    public long length() {
+        try {
+            if (isCluster(groupName)) {
+                return getBinaryJedisClusterCommands(groupName).hlen(key);
+            } else {
+                return getBinaryJedisCommands(groupName).hlen(key);
+            }
+        } finally {
+            getJedisProvider(groupName).release();
+        }
+    }
+
+    /**
+     * 获取一个值
+     * 
+     * @param field
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T getOne(String field) {
+        return (T) get(field).get(field);
+    }
+
+    /**
+     * 获取多个key的值
+     * 
+     * @param fields
+     * @return
+     */
+    public <T> Map<String, T> get(String... fields) {
+        try {
+            List<byte[]> datas = null;
+            Map<String, T> result = new HashMap<>();
+
+            byte[][] encodeFileds = SafeEncoder.encodeMany(fields);
+            if (isCluster(groupName)) {
+                datas = getBinaryJedisClusterCommands(groupName).hmget(key, encodeFileds);
+            } else {
+                datas = getBinaryJedisCommands(groupName).hmget(key, encodeFileds);
+            }
+            for (int i = 0; i < fields.length; i++) {
+                result.put(fields[i], (T) valueDerialize(datas.get(i)));
+            }
+            return result;
+        } finally {
+            getJedisProvider(groupName).release();
+        }
+
+    }
 
 }
--- azeroth-cache/src/main/java/cn/com/warlock/cache/redis/JedisProviderFactory.java
+++ azeroth-cache/src/main/java/cn/com/warlock/cache/redis/JedisProviderFactory.java
@@ -25,88 +25,94 @@ import redis.clients.jedis.MultiKeyJedisClusterCommands;
  */
 public class JedisProviderFactory {
 
-	protected static final Logger logger = LoggerFactory.getLogger(JedisProviderFactory.class);
+    protected static final Logger             logger         = LoggerFactory
+        .getLogger(JedisProviderFactory.class);
 
-	private static JedisProvider<?, ?> defaultJedisProvider;
-	
-	@SuppressWarnings("rawtypes")
-	private static Map<String, JedisProvider> jedisProviders = new ConcurrentHashMap<>();
+    private static JedisProvider<?, ?>        defaultJedisProvider;
 
-	public static JedisProvider<?, ?> getJedisProvider(String groupName) {
-		if(defaultJedisProvider == null){			
-			initFactoryFromSpring();
-		}
-		if(StringUtils.isNotBlank(groupName)){
-			if(jedisProviders.containsKey(groupName)){				
-				return jedisProviders.get(groupName);
-			}else{
-				logger.warn("未找到group[{}]对应的redis配置，使用默认缓存配置",groupName);
-			}
-		}
-		return defaultJedisProvider;
-	}
+    @SuppressWarnings("rawtypes")
+    private static Map<String, JedisProvider> jedisProviders = new ConcurrentHashMap<>();
 
-	@SuppressWarnings("rawtypes")
-	private synchronized static void initFactoryFromSpring() {
-		if(defaultJedisProvider == null){
-			//阻塞，直到spring初始化完成
-			InstanceFactory.waitUtilInitialized();
-			Map<String, JedisProvider> interfaces = InstanceFactory.getInstanceProvider().getInterfaces(JedisProvider.class);
-			Iterator<JedisProvider> iterator = interfaces.values().iterator();
-			while(iterator.hasNext()){
-				JedisProvider jp = iterator.next();
-				jedisProviders.put(jp.groupName(), jp);
-			}
-			defaultJedisProvider = jedisProviders.get(JedisProviderFactoryBean.DEFAULT_GROUP_NAME);
-			if(defaultJedisProvider == null && jedisProviders.size() == 1){
-				defaultJedisProvider = InstanceFactory.getInstance(JedisProvider.class);
-			}
-			
-			Assert.notNull(defaultJedisProvider,"无默认缓存配置，请指定一组缓存配置group为default");
-		}
-	}
+    public static void setDefaultJedisProvider(JedisProvider<?, ?> defaultJedisProvider) {
+        JedisProviderFactory.defaultJedisProvider = defaultJedisProvider;
+    }
 
-	public static JedisCommands getJedisCommands(String groupName) {
-		return (JedisCommands) getJedisProvider(groupName).get();
-	}
+    public static JedisProvider<?, ?> getJedisProvider(String groupName) {
+        if (defaultJedisProvider == null) {
+            initFactoryFromSpring();
+        }
+        if (StringUtils.isNotBlank(groupName)) {
+            if (jedisProviders.containsKey(groupName)) {
+                return jedisProviders.get(groupName);
+            } else {
+                logger.warn("未找到group[{}]对应的redis配置，使用默认缓存配置", groupName);
+            }
+        }
+        return defaultJedisProvider;
+    }
 
-	public static BinaryJedisCommands getBinaryJedisCommands(String groupName) {
-		return (BinaryJedisCommands) getJedisProvider(groupName).getBinary();
-	}
+    @SuppressWarnings("rawtypes")
+    private synchronized static void initFactoryFromSpring() {
+        if (defaultJedisProvider == null) {
+            //阻塞，直到spring初始化完成
+            InstanceFactory.waitUtilInitialized();
+            Map<String, JedisProvider> interfaces = InstanceFactory.getInstanceProvider()
+                .getInterfaces(JedisProvider.class);
+            Iterator<JedisProvider> iterator = interfaces.values().iterator();
+            while (iterator.hasNext()) {
+                JedisProvider jp = iterator.next();
+                jedisProviders.put(jp.groupName(), jp);
+            }
+            defaultJedisProvider = jedisProviders.get(JedisProviderFactoryBean.DEFAULT_GROUP_NAME);
+            if (defaultJedisProvider == null && jedisProviders.size() == 1) {
+                defaultJedisProvider = InstanceFactory.getInstance(JedisProvider.class);
+            }
 
-	public static BinaryJedisClusterCommands getBinaryJedisClusterCommands(String groupName) {
-		return (BinaryJedisClusterCommands) getJedisProvider(groupName).getBinary();
-	}
+            Assert.notNull(defaultJedisProvider, "无默认缓存配置，请指定一组缓存配置group为default");
+        }
+    }
 
-	public static JedisCommands getJedisClusterCommands(String groupName) {
-		return (JedisCommands) getJedisProvider(groupName).get();
-	}
-	
-	public static MultiKeyCommands getMultiKeyCommands(String groupName) {
-		return (MultiKeyCommands) getJedisProvider(groupName).get();
-	}
-	
-	public static MultiKeyBinaryCommands getMultiKeyBinaryCommands(String groupName) {
-		return (MultiKeyBinaryCommands) getJedisProvider(groupName).get();
-	}
-	
-	public static MultiKeyJedisClusterCommands getMultiKeyJedisClusterCommands(String groupName) {
-		return (MultiKeyJedisClusterCommands) getJedisProvider(groupName).get();
-	}
-	
-	public static MultiKeyBinaryJedisClusterCommands getMultiKeyBinaryJedisClusterCommands(String groupName) {
-		return (MultiKeyBinaryJedisClusterCommands) getJedisProvider(groupName).get();
-	}
-	
-	public static String currentMode(String groupName){
-		return getJedisProvider(groupName).mode();
-	}
-	
-	public static boolean isStandalone(String groupName){
-		return JedisStandaloneProvider.MODE.equals(currentMode(groupName));
-	}
-	
-	public static boolean isCluster(String groupName){
-		return JedisClusterProvider.MODE.equals(currentMode(groupName));
-	}
+    public static JedisCommands getJedisCommands(String groupName) {
+        return (JedisCommands) getJedisProvider(groupName).get();
+    }
+
+    public static BinaryJedisCommands getBinaryJedisCommands(String groupName) {
+        return (BinaryJedisCommands) getJedisProvider(groupName).getBinary();
+    }
+
+    public static BinaryJedisClusterCommands getBinaryJedisClusterCommands(String groupName) {
+        return (BinaryJedisClusterCommands) getJedisProvider(groupName).getBinary();
+    }
+
+    public static JedisCommands getJedisClusterCommands(String groupName) {
+        return (JedisCommands) getJedisProvider(groupName).get();
+    }
+
+    public static MultiKeyCommands getMultiKeyCommands(String groupName) {
+        return (MultiKeyCommands) getJedisProvider(groupName).get();
+    }
+
+    public static MultiKeyBinaryCommands getMultiKeyBinaryCommands(String groupName) {
+        return (MultiKeyBinaryCommands) getJedisProvider(groupName).get();
+    }
+
+    public static MultiKeyJedisClusterCommands getMultiKeyJedisClusterCommands(String groupName) {
+        return (MultiKeyJedisClusterCommands) getJedisProvider(groupName).get();
+    }
+
+    public static MultiKeyBinaryJedisClusterCommands getMultiKeyBinaryJedisClusterCommands(String groupName) {
+        return (MultiKeyBinaryJedisClusterCommands) getJedisProvider(groupName).get();
+    }
+
+    public static String currentMode(String groupName) {
+        return getJedisProvider(groupName).mode();
+    }
+
+    public static boolean isStandalone(String groupName) {
+        return JedisStandaloneProvider.MODE.equals(currentMode(groupName));
+    }
+
+    public static boolean isCluster(String groupName) {
+        return JedisClusterProvider.MODE.equals(currentMode(groupName));
+    }
 }
